ac60b7870efb85ad027ae66dee718e1062977b41
lingleiming <120040573+lingleiming@users.noreply.github.com>
2022-12-29 22:30:24 +0800

    Create new file

---
+++ kugoul.js
@@ -0,0 +1,34 @@
+/*******************************
+[rewrite_local]
+^http[s]?:\/\/gateway.kugou.com.+$ url script-response-body https://raw.githubusercontent.com/lingleiming/hcc/main/kg.js
+[mitm] 
+hostname = *.kugou.*
+*******************************/
+var obj = JSON.parse($response.body);
+    obj.user_type= 1;
+obj.user_y_type= 1;
+obj.is_vip= 1;
+obj.vip_type= 1;
+obj.vip_type= 1;
+obj.svip_score= 1;
+obj.svip_level= 1;
+obj.annual_fee_begin_time= 09";
+obj.annual_fee_end_time= 09";
+obj.su_vip_end_time= 09";
+obj.vip_end_time= 09";
+obj.roam_type= 1;
+obj.roam_end_time= 36";
+obj.m_y_endtime= 36";
+obj.vip_y_endtime= 36";
+obj.m_type= 1;
+obj.m_is_old= 1;
+obj.su_vip_y_endtime= 36";
+obj.roam_begin_time= 36";
+obj.y_type= 1;
+obj.signed_svip_before= 1;
+obj.end_time= 36";
+obj.duration= 873616277659;
+obj.p_grade= 20;
+obj.p_current_point= 873616277659;
+obj.p_next_grade= 20;
+    $done({body: JSON.stringify(obj)});
