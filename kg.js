00fb6898eb43a9475690bda58cc9580553752358
lingleiming <120040573+lingleiming@users.noreply.github.com>
2022-12-29 22:26:36 +0800

    Update kg.js

--- kg.js
+++ kg.js
@@ -1,25 +1,8 @@
 /*******************************
-  公众号:木木IOS分享
-关注了解更多新科技！！！
-酷狗
-脚本名称:酷狗
-使用声明：️此脚本仅供学习与交流，
-        请勿转载与贩卖！️️️
-群1077223830
-*******************************
 [rewrite_local]
-^http[s]?:\/\/gateway.kugou.com.+$ url script-response-body kugou.js
+^http[s]?:\/\/gateway.kugou.com.+$ url script-response-body https://raw.githubusercontent.com/lingleiming/hcc/main/kg.js
 [mitm] 
 hostname = *.kugou.*
-*******************************
-Surge
-
-[Script]
-^http[s]?:\/\/gateway.kugou.com.+$ requires-body=1,max-size=0,script-path=kugou.js
-
-[MITM]
-hostname = *.kugou.*
-
 *******************************/
 var obj = JSON.parse($response.body);
     obj.user_type= 1;
